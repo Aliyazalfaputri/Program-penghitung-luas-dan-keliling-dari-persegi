0a25e5a0504c3ffa73f6216d452ce01592ce5e95
Aliyazalfaputri <112566366+Aliyazalfaputri@users.noreply.github.com>
2022-09-08 17:33:54 +0700

    Add files via upload

---
+++ PRAKTIKUM ALIYA ZALFA PUTRI 2217051042.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int sisi,luas,keliling;
+	
+	cout<<"masukkan sisi : ";
+	cin>> sisi;
+	
+	luas=sisi*sisi;
+	cout<<"luas persegi : "<<luas<<endl;
+	
+	keliling=4*sisi;
+	cout<<"keliling persegi : "<<keliling;
+	return 0;
+}
